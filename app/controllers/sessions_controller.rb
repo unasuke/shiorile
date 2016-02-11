e38b3d53726bebfa791c1c17b121f8c658d7f5be
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-02-12 01:12:38 +0900

    Create sessions_controller
    and define sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to '/', notice: "sign in"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/', notice: "sign out"
+  end
 end
