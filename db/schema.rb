273f9aa23ba474fd4c6973a734a0a0b56e784b56
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-02-12 00:30:50 +0900

    Add column to authenticate user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211132932) do
+ActiveRecord::Schema.define(version: 20160211152141) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20160211132932) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
