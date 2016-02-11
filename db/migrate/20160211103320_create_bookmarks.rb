b939e90941e2cdce8ada2c6b9ae98946dc2a2a9e
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-02-11 19:33:29 +0900

    rails g model bookmark user_id:integer:index

---
+++ db/migrate/20160211103320_create_bookmarks.rb
@@ -0,0 +1,10 @@
+class CreateBookmarks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookmarks do |t|
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :bookmarks, :user_id
+  end
+end
