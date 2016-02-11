d5e672fc471e3e4cda213f1a6f89921910f8a6d4
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-02-12 01:13:35 +0900

    Create user.create_with_omniauth method in user model
    create user from twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.create_with_omniauth(auth)
+    @user = User.new(
+                  name: auth['info']['name'],
+                  provider: auth['provider'],
+                  uid: auth['uid'],
+                  email: "#{auth.uid}-#{auth.provider}@example.com",
+                  password: Devise.friendly_token
+                  )
+    @user.save
+    @user
+  end
+
 end
