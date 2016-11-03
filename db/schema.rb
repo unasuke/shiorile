d11ab5050262bfd281be06357f8c8c6415869138
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-11-03 22:56:17 +0900

    Remove name column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103131228) do
+ActiveRecord::Schema.define(version: 20161103135405) do
 
   create_table "auths", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20161103131228) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
