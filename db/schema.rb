dec16f0a3375900d6666c8e4a340669e4c1d05fd
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-11-03 22:26:51 +0900

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211103413) do
+ActiveRecord::Schema.define(version: 20161103131228) do
+
+  create_table "auths", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "screen_name"
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_auths_on_user_id"
+  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id"
